7c369540808a7a2c8d5f0492943068cdfb11b204
Your Name <you@example.com>
2023-09-12 23:16:47 +0700

    Task16.7-add-countCharacterInAString

--- Task11.7.js
+++ Task11.7.js
@@ -1,6 +1,12 @@
-function reverseString (Paramstring){
-    let splitString = Paramstring.split()
-    let reverseString = splitString.reverse("");
+function reverseString (Paramstring){ 
+    let splitString = Paramstring.split("")
+    let reverseString = splitString.reverse();
     let joinString = reverseString.join("")
     return joinString
+}
+function countCharacter (paramString,paramCharacter){ debugger
+    let String = paramString
+    let SplitString = String.split(paramCharacter).length-1
+    console.log(SplitString)
+    return SplitString
 }
